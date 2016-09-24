47949647c9b01ee04e643cf5f6fb0270315c6011
shikisama <569748110@qq.com>
2016-09-24 11:19:34 +0800

    删除部分多余代码

--- routes/index.js
+++ routes/index.js
@@ -50,30 +50,5 @@ router.get('/list', function(req, res, next) {
                });
 });
 
-router.get('/news', function(req, res, next) {
-  superagent.get('https://cnodejs.org/')
-      .end(function(err, sres) {
-        // 常规的错误处理
-        if (err) {
-          return next(err);
-        }
-        // sres.text 里面存储着网页的 html 内容，将它传给 cheerio.load 之后
-        // 就可以得到一个实现了 jquery 接口的变量，我们习惯性地将它命名为 `$`
-        // 剩下就都是 jquery 的内容了
-        var $ = cheerio.load(sres.text);
-        var items = [];
-        $('#topic_list .topic_title')
-            .each(function(idx, element) {
-              var $element = $(element);
-              items.push({
-                title : $element.attr('title'),
-                href : $element.attr('href')
-              });
-            });
-
-        res.send(items);
-      });
-});
-router.post('/', function(req, res, next) { res.send('get a post request'); });
 
 module.exports = router;
